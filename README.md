94f7b02772aad1de35fbb79d98b0fd953fd859c3
DanielsDubstep35 <Danielmendes2@icloud.com>
2024-05-02 18:25:10 +0100

    Added music and particles
    Co-Authored-By: Lazyice1172 <78085794+Lazyice1172@users.noreply.github.com>
    Co-Authored-By: Keith <98464156+Kylreia@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # Flocking Simulation
- 
+
 Link to our resources: https://drive.google.com/drive/folders/1oUlCEtrgKMn2k3R5RPPJNN846DZGdXq-?usp=sharing
 
 // Bug: Lags when boids lag when potentially getting too close?
+
+NameName: Keith Rumbaua, Ho Chung, Daniel Mendes
+
+Student Number: C20463336, C20348256, C20489272
+
+Class Group: TU858
+
+## Description of the project
+This project is a simple Flocking simulation game, which the bird will separate when they too close, try to get close to each other when they meet other bird, and they also will face the same direction to move.
+
+## How it work
+To sucess the flocking simulation, we need three function, alignment, cohesion, separation.
+
+Alignemt
+This funciton is used to make the bird facing the same direction
+- First, we get all the bird units volecity
+- Second, we get the average of the volecity and multiple with the MAX speed. To prevent it go faster and faster, it also subtrast their orignal volecity to reset their speed.
+
+Cohension
+This function will make the birds slowly to get close together
+- First, we get all the position of the birds
+- Second, we find the center position with the birds and normlized this result to get the direction
+
+Separation
+This function is used to make the bird try to move away from other bird
+- We get the dist between the bird itself and other birds
+- Finally, we get the avarage of the separation and normalized this result to get the volecity
+
+At the end
+We add these 3 results with the Weight to get final volecity
--- free_look_camera.gd
+++ free_look_camera.gd
@@ -55,89 +55,91 @@ func _input(event):
 			MOUSE_BUTTON_WHEEL_DOWN: # decrease fly velocity
 				_velocity = clamp(_velocity / speed_scale, min_speed, max_speed)
 
-	#if Input.is_physical_key_pressed(KEY_Z) and following and camera_to_creature_timer.is_stopped() and !follow_button_pressed:
-		#camera_to_creature_timer.start()
-		#follow_button_pressed = true
-		#following = false
-		##print("Follow Released!")
-	#elif Input.is_key_pressed(KEY_Z) and !following and camera_to_creature_timer.is_stopped() and !follow_button_pressed:
-		#original_camera_switch_position = camera.global_position
-		#camera_to_creature_timer.start()
-		#follow_button_pressed = true
-		#following = true
-		##print("A Creature is now being followed!")
+	if Input.is_physical_key_pressed(KEY_Z) and following and camera_to_creature_timer.is_stopped() and !follow_button_pressed:
+		camera_to_creature_timer.start()
+		follow_button_pressed = true
+		following = false
+		#print("Follow Released!")
+	elif Input.is_key_pressed(KEY_Z) and !following and camera_to_creature_timer.is_stopped() and !follow_button_pressed:
+		original_camera_switch_position = camera.global_position
+		camera_to_creature_timer.start()
+		follow_button_pressed = true
+		following = true
+		#print("A Creature is now being followed!")
 		
-	#if Input.is_physical_key_pressed(KEY_X) and !time_button_pressed and !time_stopped:
-		#time_button_pressed = true
-		#time_stop()
-	#elif Input.is_physical_key_pressed(KEY_X) and !time_button_pressed and time_stopped:
-		#time_button_pressed = true
-		#time_resume()
+	if Input.is_physical_key_pressed(KEY_X) and !time_button_pressed and !time_stopped:
+		time_button_pressed = true
+		time_stop()
+	elif Input.is_physical_key_pressed(KEY_X) and !time_button_pressed and time_stopped:
+		time_button_pressed = true
+		time_resume()
 
-#func follow(lerp_timeline, lerp_timeline_rotation):
-#
-	## The goal is to call look_at constantly in a look towards the mother bird.
+func follow(lerp_timeline, lerp_timeline_rotation):
+
+	# The goal is to call look_at constantly in a look towards the mother bird.
 	#slow_rotate_to_target(random_boid.global_position, lerp_timeline_rotation)
-#
+
 	#camera.global_position = lerp(original_camera_switch_position, camera_marker.global_position, lerp_timeline)
-#
-#func unfollow(lerp_timeline):
+	camera.global_position = camera_marker.global_position
+
+func unfollow(lerp_timeline):
 	#camera.global_position = lerp(camera_marker.global_position, original_camera_switch_position, lerp_timeline)
-#
-#func slow_rotate_to_target(target, lerp_timeline):
-	#var previous_value = Vector3.ZERO
-	#var next_value = Vector3i.ONE
-	#var forward = target - camera.global_transform.origin
-	#forward = forward.normalized() * -1
-#
-	#var right = Vector3.UP.cross(forward).normalized()
-	#var up = forward.cross(right).normalized()
-#
-	#var new_basis = Basis(right, up, forward)
-	#var new_rotation = new_basis.get_euler()
-	#var current_rotation = camera.global_transform.basis.get_euler()
-	#var final_rotation = current_rotation.cubic_interpolate(new_rotation, previous_value, next_value, lerp_timeline)
-	#camera.global_rotation = final_rotation
-#
-#func time_stop():
-	#Engine.time_scale = 0.15
-	#time_stopped = true
-	#time_button_pressed = false
-	#
-#func time_resume():
-	#Engine.time_scale = 1
-	#time_stopped = false
-	#time_button_pressed = false
+	camera.global_position = original_camera_switch_position
+
+func slow_rotate_to_target(target, lerp_timeline):
+	var previous_value = Vector3.ZERO
+	var next_value = Vector3i.ONE
+	var forward = target - camera.global_transform.origin
+	forward = forward.normalized() * -1
+
+	var right = Vector3.UP.cross(forward).normalized()
+	var up = forward.cross(right).normalized()
+
+	var new_basis = Basis(right, up, forward)
+	var new_rotation = new_basis.get_euler()
+	var current_rotation = camera.global_transform.basis.get_euler()
+	var final_rotation = current_rotation.cubic_interpolate(new_rotation, previous_value, next_value, lerp_timeline)
+	camera.global_rotation = final_rotation
+
+func time_stop():
+	Engine.time_scale = 0.15
+	time_stopped = true
+	time_button_pressed = false
+	
+func time_resume():
+	Engine.time_scale = 1
+	time_stopped = false
+	time_button_pressed = false
 
 func _process(delta):
 
-	#var duration = float(duration_to_start_control - camera_to_creature_timer.time_left)
-	#var lerp_timeline = Tween.interpolate_value(0.0, 1.0, duration, duration_to_start_control, Tween.TRANS_EXPO, Tween.EASE_OUT)
-	#var lerp_timeline_rotation = Tween.interpolate_value(0.0, 1.0, duration, duration_to_start_control, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-#
-	##print("Lerp timeline is: (" + str(lerp_timeline) + ")")
-#
-	#if following and !camera_to_creature_timer.is_stopped() and follow_button_pressed:
-		#follow_button_pressed = false
-#
-		#var num_of_boids = $"../../../../Manager".get_num_of_units()
-		#var random_boid_chooser = randi() % num_of_boids
-		#random_boid = get_tree().get_nodes_in_group("boid")[random_boid_chooser]
-		#var look_at_steps_control = 100
-#
-		#for node in random_boid.get_children():
-			#if node.is_in_group("marker"):
-				##print("camera marker found!")
-				#camera_marker = node
-#
-		## if self.is_in_group("mother"):
-		## 	camera.look_at(self.global_position, Vector3.UP)
-		#followTriggered = true
-		#follow(lerp_timeline, lerp_timeline_rotation)
-	#elif !following and !camera_to_creature_timer.is_stopped() and follow_button_pressed:
-		#follow_button_pressed = false
-		#unfollowTriggered = true
-		#unfollow(lerp_timeline)
+	var duration = float(duration_to_start_control - camera_to_creature_timer.time_left)
+	var lerp_timeline = Tween.interpolate_value(0.0, 1.0, duration, duration_to_start_control, Tween.TRANS_EXPO, Tween.EASE_OUT)
+	var lerp_timeline_rotation = Tween.interpolate_value(0.0, 1.0, duration, duration_to_start_control, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+
+	#print("Lerp timeline is: (" + str(lerp_timeline) + ")")
+
+	if following and !camera_to_creature_timer.is_stopped() and follow_button_pressed:
+		follow_button_pressed = false
+
+		var num_of_boids = $"../../../../Manager".get_num_of_units()
+		var random_boid_chooser = randi() % num_of_boids
+		random_boid = get_tree().get_nodes_in_group("boid")[random_boid_chooser]
+		var look_at_steps_control = 100
+
+		for node in random_boid.get_children():
+			if node.is_in_group("marker"):
+				#print("camera marker found!")
+				camera_marker = node
+
+		# if self.is_in_group("mother"):
+		# 	camera.look_at(self.global_position, Vector3.UP)
+		followTriggered = true
+		follow(lerp_timeline, lerp_timeline_rotation)
+	elif !following and !camera_to_creature_timer.is_stopped() and follow_button_pressed:
+		follow_button_pressed = false
+		unfollowTriggered = true
+		unfollow(lerp_timeline)
 
 	if not current:
 		return
@@ -153,12 +155,12 @@ func _process(delta):
 	else:
 		translate(direction * _velocity * delta)
 
-	#if lerp_timeline < 1.0 && followTriggered:
-		#follow(lerp_timeline, lerp_timeline_rotation)
-	#else:
-		#followTriggered = false
-#
-	#if lerp_timeline < 1.0 && unfollowTriggered:
-		#unfollow(lerp_timeline)
-	#else:
-		#unfollowTriggered = false
+	if lerp_timeline < 1.0 && followTriggered:
+		follow(lerp_timeline, lerp_timeline_rotation)
+	else:
+		followTriggered = false
+
+	if lerp_timeline < 1.0 && unfollowTriggered:
+		unfollow(lerp_timeline)
+	else:
+		unfollowTriggered = false
